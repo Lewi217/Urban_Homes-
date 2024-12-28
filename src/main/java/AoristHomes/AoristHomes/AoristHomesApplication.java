7adc4062878b4fa3ad4355f2c4a89f1bc6b9ea16
wanjohi lewi <wanjohilewi7@gmail.com>
2024-12-28 22:19:43 +0300

    init commit

---
+++ src/main/java/AoristHomes/AoristHomes/AoristHomesApplication.java
@@ -0,0 +1,13 @@
+package AoristHomes.AoristHomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoristHomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoristHomesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AoristHomes/AoristHomes/AoristHomesApplicationTests.java
@@ -0,0 +1,13 @@
+package AoristHomes.AoristHomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoristHomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
