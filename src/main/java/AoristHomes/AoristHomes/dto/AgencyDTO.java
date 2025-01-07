4e0e7636195bef8ec9a3871244f6e8ec9efd178a
wanjohi lewi <wanjohilewi7@gmail.com>
2025-01-07 23:52:09 +0300

    changed the agency model and DTO

--- src/main/java/AoristHomes/AoristHomes/dto/AgencyDTO.java
+++ src/main/java/AoristHomes/AoristHomes/dto/AgencyDTO.java
@@ -1,10 +1,18 @@
 package AoristHomes.AoristHomes.dto;
 
 import lombok.Data;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class AgencyDTO {
     private String id;
     private String name;
-    private String email;
+    private String description;
+    private BigDecimal walletBalance;
+    private List<String> propertyIds;
+
 }
--- src/main/java/AoristHomes/AoristHomes/model/Agency.java
+++ src/main/java/AoristHomes/AoristHomes/model/Agency.java
@@ -2,10 +2,12 @@ package AoristHomes.AoristHomes.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,8 +16,13 @@ public class Agency {
     @Id
     private String id;
     private String name;
-    private String email;
-    private String password;
+    private String description;
+    private BigDecimal walletBalance;
+    private List<String> propertyIds;
+
+    @CreatedDate
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
     private LocalDateTime updatedAt = LocalDateTime.now();
 }
