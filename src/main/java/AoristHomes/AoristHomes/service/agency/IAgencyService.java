85ee9fceca197088f3054414934a7c91eec297c1
wanjohi lewi <wanjohilewi7@gmail.com>
2025-01-10 18:03:18 +0300

    added agency service and an agency interface

--- src/main/java/AoristHomes/AoristHomes/service/agency/IAgencyService.java
+++ src/main/java/AoristHomes/AoristHomes/service/agency/IAgencyService.java
@@ -1,4 +1,16 @@
 package AoristHomes.AoristHomes.service.agency;
 
+import AoristHomes.AoristHomes.dto.AgencyDTO;
+import AoristHomes.AoristHomes.dto.PropertyDTO;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface IAgencyService {
+    AgencyDTO createAgency(AgencyDTO agencyDTO);
+    AgencyDTO getAgencyById(String id);
+    AgencyDTO updateAgency(String id, AgencyDTO agencyDTO);
+    boolean deleteAgency(String id);
+    boolean withdrawFromWallet(String agencyId, BigDecimal amount);
+    List<PropertyDTO> listPropertiesForInvestment(String agencyId);
 }
