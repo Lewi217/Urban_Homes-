ece7ed5d3046e02a283d7c817c2e2d94805358f1
wanjohi lewi <wanjohilewi7@gmail.com>
2026-02-20 16:56:49 +0300

    fixed admin issues

--- src/main/java/AoristHomes/AoristHomes/service/user/CustomUserDetailsService.java
+++ src/main/java/AoristHomes/AoristHomes/service/user/CustomUserDetailsService.java
@@ -1,28 +1,47 @@
 package AoristHomes.AoristHomes.service.user;
 
+import AoristHomes.AoristHomes.model.Admin;
 import AoristHomes.AoristHomes.model.User;
+import AoristHomes.AoristHomes.repository.AdminRepository;
 import AoristHomes.AoristHomes.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final AdminRepository adminRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+
+        Optional<Admin> adminOpt = adminRepository.findByEmail(email);
+        if (adminOpt.isPresent()) {
+            Admin admin = adminOpt.get();
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(admin.getEmail())
+                    .password(admin.getPassword())
+                    .authorities(List.of(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                    .build();
+        }
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
 
         return org.springframework.security.core.userdetails.User
                 .withUsername(user.getEmail())
                 .password(user.getPassword())
-                .authorities("USER")
+                .authorities(List.of(new SimpleGrantedAuthority("ROLE_USER")))
                 .build();
     }
 }
